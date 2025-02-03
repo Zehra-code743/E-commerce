b9a07ef0dac8c4fca1e94c6236c10f2a30b64b46
Shan-e-Zehra <shanezehra117@gmail.com>
2025-02-03 06:09:28 +0500

    errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+    eslint:{
+        ignoreDuringBuilds: true,
+        ignoreDuringDevelopments: true,
+    },
+
     images:{
         remotePatterns:[
          {
