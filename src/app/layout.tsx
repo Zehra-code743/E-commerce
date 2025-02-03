12224aaf4b05d5c399044ee1d8c091dca8a9e1f4
Shan-e-Zehra <shanezehra117@gmail.com>
2025-02-03 21:52:44 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import Navbar from "@/Components/navbar";
 import Hero from "@/Components/Hero";
 import NewCollections from "@/Components/Newcollections.";
